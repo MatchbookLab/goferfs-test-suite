92d43fa32dae610ccfba1c6a0666d2bf52fbeb2c
Cameron Spear <cameronspear@gmail.com>
2017-01-23 23:31:34 -0800

    Update to test with goferfs@types

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,75 @@
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 
-import { IAdapter, IFilesystem } from 'goferfs-types/interfaces';
-import { Visibility } from 'goferfs-types';
-const Gofer: IFilesystem = require('goferfs');
+import Gofer from 'goferfs';
+import { IAdapter } from 'goferfs/types/interfaces';
+import { Visibility } from 'goferfs/types';
 import { Readable } from 'stream';
 
 chai.use(chaiAsPromised);
 chai.should();
 
 export default function(adapter: IAdapter) {
-    const filesystem = new Gofer(adapter);
+    const gofer = new Gofer(adapter);
 
     describe('The Basics', function () {
         it('should write and read', async () => {
-            await filesystem.write('test.txt', 'Hello, friend!');
+            await gofer.write('test.txt', 'Hello, friend!');
 
-            (await filesystem.exists('test.txt')).should.equal(true);
+            (await gofer.exists('test.txt')).should.equal(true);
 
-            const { contents } = await filesystem.read('test.txt');
+            const { contents } = await gofer.read('test.txt');
             contents.should.equal('Hello, friend!');
         });
 
         it('should delete', async () => {
-            await filesystem.delete('test.txt');
+            await gofer.delete('test.txt');
 
-            (await filesystem.exists('test.txt')).should.equal(false);
+            (await gofer.exists('test.txt')).should.equal(false);
         });
 
         it('should create path to file', async () => {
-            await filesystem.write('path/to/test.txt', 'Test');
+            await gofer.write('path/to/test.txt', 'Test');
 
-            (await filesystem.exists('path/to/test.txt')).should.equal(true);
+            (await gofer.exists('path/to/test.txt')).should.equal(true);
         });
 
         it('should delete a directory', async () => {
-            await filesystem.deleteDir('path');
+            await gofer.deleteDir('path');
 
-            (await filesystem.exists('path/to/test.txt')).should.equal(false);
+            (await gofer.exists('path/to/test.txt')).should.equal(false);
         });
     });
 
     describe('The Fun', function () {
         afterEach(async () => {
-            await filesystem.deleteDir('/');
+            await gofer.deleteDir('/');
 
-            (await filesystem.exists('path')).should.equal(false);
+            (await gofer.exists('path')).should.equal(false);
         });
 
         it('should rename', async () => {
-            await filesystem.write('path/to/test1.txt', 'Test');
-            await filesystem.move('path/to/test1.txt', 'path/to/test2.txt');
+            await gofer.write('path/to/test1.txt', 'Test');
+            await gofer.move('path/to/test1.txt', 'path/to/test2.txt');
 
-            (await filesystem.exists('path/to/test1.txt')).should.equal(false);
-            (await filesystem.exists('path/to/test2.txt')).should.equal(true);
-            (await filesystem.read('path/to/test2.txt')).contents.should.equal('Test');
+            (await gofer.exists('path/to/test1.txt')).should.equal(false);
+            (await gofer.exists('path/to/test2.txt')).should.equal(true);
+            (await gofer.read('path/to/test2.txt')).contents.should.equal('Test');
         });
 
         it('should copy', async () => {
-            await filesystem.write('path/to/test1.txt', 'Test');
-            await filesystem.copy('path/to/test1.txt', 'path/to/test2.txt');
+            await gofer.write('path/to/test1.txt', 'Test');
+            await gofer.copy('path/to/test1.txt', 'path/to/test2.txt');
 
-            (await filesystem.exists('path/to/test1.txt')).should.equal(true);
-            (await filesystem.exists('path/to/test2.txt')).should.equal(true);
-            (await filesystem.read('path/to/test1.txt')).contents.should.equal('Test');
-            (await filesystem.read('path/to/test2.txt')).contents.should.equal('Test');
+            (await gofer.exists('path/to/test1.txt')).should.equal(true);
+            (await gofer.exists('path/to/test2.txt')).should.equal(true);
+            (await gofer.read('path/to/test1.txt')).contents.should.equal('Test');
+            (await gofer.read('path/to/test2.txt')).contents.should.equal('Test');
         });
 
         it('should return full metadata', async () => {
-            await filesystem.write('path/to/test1.txt', 'Test');
-            const file = await filesystem.read('path/to/test1.txt');
+            await gofer.write('path/to/test1.txt', 'Test');
+            const file = await gofer.read('path/to/test1.txt');
 
             file.contents.should.equal('Test');
             file.name.should.equal('test1.txt');
@@ -90,9 +90,9 @@ export default function(adapter: IAdapter) {
             inputStream.push('Test Stream');
             inputStream.push(null);
 
-            await filesystem.writeStream('path/to/test1.txt', inputStream);
+            await gofer.writeStream('path/to/test1.txt', inputStream);
 
-            const { stream } = await filesystem.readStream('path/to/test1.txt');
+            const { stream } = await gofer.readStream('path/to/test1.txt');
 
             const contents = await new Promise((resolve, reject) => {
                 let str = '';
@@ -105,16 +105,16 @@ export default function(adapter: IAdapter) {
         });
 
         it('should get visibility', async () => {
-            await filesystem.write('path/to/test.txt', 'Test');
+            await gofer.write('path/to/test.txt', 'Test');
 
-            (await filesystem.getVisibility('path/to/test.txt')).should.equal(Visibility.Public);
+            (await gofer.getVisibility('path/to/test.txt')).should.equal(Visibility.Public);
         });
 
         it('should get visibility', async () => {
-            await filesystem.write('path/to/test.txt', 'Test');
+            await gofer.write('path/to/test.txt', 'Test');
 
-            await filesystem.setVisibility('path/to/test.txt', Visibility.Private);
-            (await filesystem.getVisibility('path/to/test.txt')).should.equal(Visibility.Private);
+            await gofer.setVisibility('path/to/test.txt', Visibility.Private);
+            (await gofer.getVisibility('path/to/test.txt')).should.equal(Visibility.Private);
         });
     });
 }
